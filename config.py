4f53a0454e53d15a1315a73cfeaaf86f49eaecba
akpandawep <154840412+akpandawep@users.noreply.github.com>
2024-04-04 03:27:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import re
 import os
 from os import getenv, environ
 
-emojis = ["👍", "🔥","👌", "⚡", "🏆", "👻", "👨‍💻", "👀", "✅",]
+emojis = ["👍", "🔥","👌", "⚡", "🏆", "👻", "👨‍💻",]
 api_id = int(environ.get("API_ID", "29917436"))
 api_hash = environ.get("API_HASH", "4a926822b076a086a167fe8f2701d3e9")
 bot_token = environ.get("BOT_TOKEN", "7184162011:AAHe4OY7VFFhpMqgiBlLcVDVuQNJnqMWBnQ")
