66440b3c4e4261e31c7a518c309074d721c335d6
akpandawep <154840412+akpandawep@users.noreply.github.com>
2024-04-04 03:47:08 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,29 +1,18 @@
 from pyrogram import Client, idle
 from plugins.Ashu import app as Client2
 from config import *
-
-
-
 bot = Client(
-
            "Renamer",
-
            bot_token=bot_token,
-
            api_id=api_id,
-
            api_hash=api_hash,
-
            plugins=dict(root='plugins'))
-           
-
-if ASHUBHIRUNTHISBOT:
+    if ASHUBHIRUNTHISBOT:
     apps = [Client2,bot]
     for app in apps:
         app.start()
     idle()
     for app in apps:
         app.stop()
-    
 else:
     bot.run()
